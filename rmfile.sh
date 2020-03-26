04c1e3e8bdd73964b592c1195704e82cd7a83304
Tim Urista <timothy.urista@gmail.com>
2020-03-26 09:37:12 -0700

    added rm file

--- rmfile.sh
+++ rmfile.sh
@@ -1,4 +1,4 @@
 git filter-branch --index-filter \
-'git update-index --remove $1' HEAD^1..HEAD
-git push --force --verbose --dry-run
+'git update-index --remove $1' HEAD^1..HEAD \
+git push --force --verbose --dry-run \
 git push --force
